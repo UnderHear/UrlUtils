0083da1da761b7ea09ed9487eaac5eb69f87e622
UnderHear <2533643340@qq.com>
2026-06-05 00:14:38 +0800

    首次提交

---
+++ UrlUtils.java
@@ -0,0 +1,12 @@
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public final class UrlUtils {
+
+	private UrlUtils() {
+	}
+
+	public static void print123() {
+		System.out.println(123);
+	}
+}
